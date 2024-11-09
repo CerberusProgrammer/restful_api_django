3b7c1c601bcc3b120803bef396d6aad7365800e1
sazardev <cerberusprogrammer@gmail.com>
2024-11-09 12:52:12 -0800

    Refactor API structure: add ProductFilter class for filtering products by various fields

--- logistico/product/product_views.py
+++ logistico/product/product_views.py
@@ -1,9 +1,12 @@
-# views
-from django.shortcuts import render
 from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+
+from logistico.product.product_filters import ProductFilter
 from .product import Product
 from .product_serializers import ProductSerializer
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = ProductFilter
