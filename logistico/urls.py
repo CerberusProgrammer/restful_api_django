dfdaf821273abbbe238a7c7bdc3b8f4cdd3b502f
sazardev <cerberusprogrammer@gmail.com>
2024-11-09 12:36:47 -0800

    Refactor API structure: remove unnecessary API files and update URL routing

--- logistico/urls.py
+++ logistico/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('products/', include('logistico.product.product_urls')),
-    path('subastas/', include('logistico.subasta.subasta_urls')),
-    path('viajes/', include('logistico.viaje.viaje_urls')),
+    path('', include('logistico.product.product_urls')),
 ]
