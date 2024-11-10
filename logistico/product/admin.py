61f2c0146db079c1886d5543d044ba1527722d53
sazardev <cerberusprogrammer@gmail.com>
2024-11-09 21:44:45 -0800

    Refactor admin configuration for Product model

--- logistico/product/admin.py
+++ logistico/product/admin.py
@@ -5,3 +5,7 @@ from .product import Product
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'stock', 'state', 'creation_date', 'modification_date')
     search_fields = ('name', 'description')
+    list_filter = ('state', 'creation_date', 'modification_date')
+    date_hierarchy = 'creation_date'
+    ordering = ('-creation_date',)
+    readonly_fields = ('creation_date', 'modification_date')
