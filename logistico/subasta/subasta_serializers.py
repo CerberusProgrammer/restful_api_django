946b9febe98a0d066dbdf04f84f27e6253bc11d2
sazardev <cerberusprogrammer@gmail.com>
2024-11-09 21:19:29 -0800

    Refactor Subasta serializers: separate into simple and detailed serializers, add products field to detailed serializer

--- logistico/subasta/subasta_serializers.py
+++ logistico/subasta/subasta_serializers.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers
+
+from logistico.product.product_serializers import ProductSerializer
 from .subasta import Subasta
 
-class SubastaSerializer(serializers.ModelSerializer):
+class SubastaSimpleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subasta
+        fields = '__all__'
+
+class SubastaDetailedSerializer(serializers.ModelSerializer):
+    products = ProductSerializer(many=True, read_only=True)
+
     class Meta:
         model = Subasta
         fields = '__all__'
