19a8806e7ca0ef4e82cd62d9e2d80b97a44d8658
sazardev <cerberusprogrammer@gmail.com>
2024-11-09 21:44:52 -0800

    Refactor admin configuration for Product model

--- logistico/subasta/admin.py
+++ logistico/subasta/admin.py
@@ -5,3 +5,7 @@ from .subasta import Subasta
 class SubastaAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'stock', 'state', 'creation_date', 'modification_date')
     search_fields = ('name', 'description')
+    list_filter = ('state', 'creation_date', 'modification_date')
+    date_hierarchy = 'creation_date'
+    ordering = ('-creation_date',)
+    readonly_fields = ('creation_date', 'modification_date')
