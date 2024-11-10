43f52987a565456b916b1344916cf84ebcbf2fe9
sazardev <cerberusprogrammer@gmail.com>
2024-11-09 21:19:34 -0800

    Refactor SubastaViewSet to support different serializer projections

--- logistico/subasta/subasta_views.py
+++ logistico/subasta/subasta_views.py
@@ -2,10 +2,11 @@ from rest_framework import viewsets
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.filters import OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.response import Response
 
 from .subasta import Subasta
 from .subasta_filters import SubastaFilter
-from .subasta_serializers import SubastaSerializer
+from .subasta_serializers import SubastaSimpleSerializer, SubastaDetailedSerializer
 
 class SubastaPagination(PageNumberPagination):
     page_size_query_param = 'size'
@@ -13,9 +14,14 @@ class SubastaPagination(PageNumberPagination):
 
 class SubastaViewSet(viewsets.ModelViewSet):
     queryset = Subasta.objects.all()
-    serializer_class = SubastaSerializer
     filter_backends = [DjangoFilterBackend, OrderingFilter]
     filterset_class = SubastaFilter
     pagination_class = SubastaPagination
     ordering_fields = ['name', 'description', 'price', 'stock', 'state', 'creation_date', 'modification_date', 'product_id']
     ordering = ['name']
+
+    def get_serializer_class(self):
+        projection = self.request.query_params.get('projection', 'simple')
+        if projection == 'detailed':
+            return SubastaDetailedSerializer
+        return SubastaSimpleSerializer
